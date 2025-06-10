7a006cc2ab46a274cf5c9d2e829b12af522b2094
F0XYW4FFL3Z <tangeriine.dreamz@gmail.com>
2025-06-09 21:33:08 -0400

    Removed toggleButtonState parameters due to redundancy.

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -59,12 +59,12 @@ class FormValidator {
       this._submitButtonSelector
     );
 
-    this._toggleButtonState(this._buttonElement);
+    this._toggleButtonState());
 
     this._inputList.forEach((inputElement) => {
       inputElement.addEventListener("input", () => {
         this._checkInputValidity(inputElement);
-        this._toggleButtonState(this._buttonElement);
+        this._toggleButtonState();
       });
     });
   }
@@ -82,7 +82,7 @@ class FormValidator {
       this._submitButtonSelector
     );
     this._formEl.reset();
-    this._toggleButtonState(this._buttonElement);
+    this._toggleButtonState();
   }
 }
 
